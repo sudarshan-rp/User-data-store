25c94bb7812fcc10b5b43f30e76d57850666a4a6
boss0077 <sudarshanrpgowda7@gmail.com>
2025-09-17 11:13:18 +0530

    instrumented for prometheus

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from backend.api.routes.health_routes import router as health_router
 from backend.api.routes.user_routes import router as user_router
 from backend.db.database import connect_to_db, disconnect_from_db, create_tables
+
+##Observability
+#from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter, Gauge, make_asgi_app
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -28,3 +33,30 @@ def create_app() -> FastAPI:
     return app
 
 app = create_app()
+
+
+#---------------------------- Observability ---------------------------
+#Instrumentator().instrument(app).expose(app)
+
+REQUEST_COUNT = Counter(
+    'http_request_count', 'Total HTTP requests', ['method', 'endpoint', 'http_status']
+)
+IN_PROGRESS = Gauge(
+    'inprogress_requests', 'Requests in progress'
+)
+
+# Middleware to update metrics per request
+@app.middleware("http")
+async def prometheus_metrics(request: Request, call_next):
+    IN_PROGRESS.inc()
+    response = await call_next(request)
+    REQUEST_COUNT.labels(
+        method=request.method,
+        endpoint=request.url.path,
+        http_status=response.status_code,
+    ).inc()
+    IN_PROGRESS.dec()
+    return response
+
+# Mount /metrics endpoint
+app.mount("/metrics", make_asgi_app())
